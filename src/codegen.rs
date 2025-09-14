a5edf4ae666b9179847a050eeca94b455e21b59c
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2025-09-14 15:08:41 -0400

    Add integer division operator a _/ b

--- src/codegen.rs
+++ src/codegen.rs
@@ -659,6 +659,7 @@ fn gen_bin_op(
         Sub => code.push(Insn::sub),
         Mul => code.push(Insn::mul),
         Div => code.push(Insn::div),
+        IntDiv => code.push(Insn::div_int),
         Mod => code.push(Insn::modulo),
 
         Eq => code.push(Insn::eq),
