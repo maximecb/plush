eede38805862d4a5f096804357f683954b819402
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2026-09-18 19:46:01 -0400

    Add sha256 to stdlib

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -20,6 +20,7 @@ pub fn get_source(unit_key: &str) -> Option<&'static str>
 
     let src = match module_path {
         "random" => include_str!("../stdlib/random.psh"),
+        "sha256" => include_str!("../stdlib/sha256.psh"),
         _ => return None
     };
 
